f88a164c1e8cc09e4657f16e201e0aae8420f3aa
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-10 13:36:42 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,8 +29,8 @@
 		click the +/- 
 	</h2>
 
-	<h2 href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-		click me
+	<h2>
+		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">click me</a>
 	</h2>
 	
 	<Counter />
